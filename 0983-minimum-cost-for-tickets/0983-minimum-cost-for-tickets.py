ef1941b3d1185ae5b1e18d25fd4f41697bff5c27
dhriti46 <dhriti.faldu126910@marwadiuniversity.ac.in>
2026-08-15 18:15:18 +0530

    Time: 7 ms (48.1%), Space: 12.3 MB (74.05%) - LeetHub

---
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.py
@@ -0,0 +1,32 @@
+class Solution:
+
+    def mincostTickets(self, days, costs):
+
+        n = len(days)
+
+        dp = [0] * (n + 1)
+
+        for i in range(n - 1, -1, -1):
+
+            # 1-day pass
+            cost1 = costs[0] + dp[i + 1]
+
+            # 7-day pass
+            j = i
+
+            while j < n and days[j] < days[i] + 7:
+                j += 1
+
+            cost7 = costs[1] + dp[j]
+
+            # 30-day pass
+            j = i
+
+            while j < n and days[j] < days[i] + 30:
+                j += 1
+
+            cost30 = costs[2] + dp[j]
+
+            dp[i] = min(cost1, cost7, cost30)
+
+        return dp[0]
